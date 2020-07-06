7b37e86f2ddfd7f88cee9b0b1ae75a0021e856bc
minh <shenlong2361996@gmail.com>
2020-07-07 00:45:52 +0200

    change class based cmp to functional cmp

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,19 @@
 import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View, TextInput, Button, FlatList, AsyncStorage, ScrollView  } from 'react-native';
+import { useEffect, useState } from 'react';
+import {
+  Image,
+  Platform,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput,
+  Button,
+  FlatList,
+  AsyncStorage,
+  ScrollView,
+} from 'react-native';
 import { CheckBox } from 'native-base';
 // import { ScrollView } from 'react-native-gesture-handler';
 import { Formik } from 'formik';
@@ -8,160 +21,141 @@ import { Formik } from 'formik';
 import { MonoText } from '../components/StyledText';
 
 import io from 'socket.io-client/dist/socket.io';
+const socket = io('http://127.0.0.1:3000'); // your router ip address here instead of localhost
 
+const HomeScreen = props => {
+  const [gameBegun, setGameBegun] = useState(false);
+  const [adminMessages, setAdminMessages] = useState([]);
+  const [id, setId] = useState();
+  const [room, setRoom] = useState();
+  const [name, setName] = useState();
+  const [users, setUsers] = useState([]);
+  const [isReady, setIsReady] = useState(false);
 
+  /* setMessages = message => {
+      setState(prevState => ({ adminMessages: prev   adminMessages.concat([message]) }));
+  }; */
 
-export default class HomeScreen extends React.Component {
-  constructor(props){
-    super(props);
-    this.socket = io('http://192.168.1.152:3000'); // your router ip address here instead of localhost
-  }
-  state = {
-    gameBegun: false,
-    adminMessages: [],
-    id: undefined,
-    room: undefined,
-    name: undefined,
-    users: [],
-    isReady: false,
-  }
+  const setMessages = message => {
+    setAdminMessages(prev => {
+      return prev.concat([message]);
+    });
+  };
+  useEffect(() => {
+    socket.on('updateUserList', users => {
+      console.log('current users', users);
 
-  setStateData = (key, value) => {
-    this.setState({ [key]: value })
-  }
-
-  setMessages = (message) => {
-    this.setState(prevState => ({ adminMessages: prevState.adminMessages.concat([message])}))
-  }
-
-  componentDidMount = () => {
-    this.socket.on('updateUserList', (users) => {
-      console.log('current users', users)
-
-      let i = 1
+      let i = 1;
       users = users.map(user => {
-        user.index = i
-        i++
-        if (user.id === this.socket.id) this.setStateData('isReady', user.isReady)
-        return user
-      })
+        user.index = i;
+        i++;
+        if (user.id === socket.id) setIsReady(user.isReady);
+        return user;
+      });
 
-      this.setStateData('users', users)
-    })
+      setUsers(users);
+    });
+  });
 
-    this.socket.on('adminMessage', (res) => {
-      console.log(res.time, res.message)
+  socket.on('adminMessage', res => {
+    console.log(res.time, res.message);
 
-      this.setMessages(res)
-      console.log(this.state.adminMessages)
-    })
+    setMessages(res);
+    console.log(adminMessages);
+  });
 
-    this.socket.on('gameData', (res) => {
-      this.setStateData('gameBegun', true)
-      console.log(res)
-    })
-  }
+  socket.on('gameData', res => {
+    setGameBegun(true);
+    console.log(res);
+  });
 
-  handleLeave = () => {
-    this.socket.emit('leave', {room: this.state.room, name: this.state.name}, () => {
-      this.setStateData('room', undefined)
-    })
-  }
+  const handleLeave = () => {
+    socket.emit('leave', { room: room, name: name }, () => {
+      setRoom(undefined);
+    });
+  };
 
-  handleReady = () => {
-    this.socket.emit('ready', { id: this.socket.id, room: this.state.room, isReady: !this.state.isReady}, () => {
-    })
-  }
+  const handleReady = () => {
+    socket.emit('ready', { id: socket.id, room: room, isReady: !isReady }, () => {});
+  };
 
-  onSubmit = (values) => {
-    console.log('on submit', values)
-    this.socket.emit('join', values, () => {
-      console.log('emit sucess!')
-      this.setStateData('room', values.room)
-      this.setStateData('name', values.name)
-      this.setStateData('isReady', false)
-    })
-  }
+  const onSubmit = values => {
+    console.log('on submit', values);
+    socket.emit('join', values, () => {
+      console.log('emit sucess!');
+      setRoom(values.room);
+      setName(values.name);
+      setIsReady(false);
+    });
+  };
 
-  render() {
-    return (
-      <View style={styles.container}>
-        {
-          this.state.gameBegun ? 
-          <View>
-              <Text>The game has begun</Text>
-              <ScrollView style={styles.messagesContainer}>
-                <FlatList 
-                  data={this.state.adminMessages}
-                  renderItem={({item}) => 
-                    <View style={styles.checkboxContainer}>
-                      <Text key={item.index}>{item.time}: {item.message} </Text>
-                    </View>
-                  }  
-                />
-              </ScrollView>
-          </View>
-            :
-            this.state.room ? 
-              <View>
-                <Text>User: {this.state.name}</Text>
-                <Text>Room: {this.state.room}</Text>
-                <Button onPress={this.handleReady} title={!this.state.isReady ? "Ready?" : "Not Ready" } />
-                <Button onPress={this.handleLeave} title="Go out"></Button>
-                <Text>List of users:</Text>
-                <FlatList 
-                  data={this.state.users}
-                  renderItem={({item}) => 
-                    <View style={styles.checkboxContainer}>
-                      <Text key={item.index}>{item.index}: {item.name} </Text>
-                      <CheckBox
-                        disabled={true}
-                        style={styles.checkBox}
-                        checked={item.isReady}
-                      />
-                    </View>}  
-                />
-                <View>
-                  <FlatList 
-                    data={this.state.adminMessages}
-                    renderItem={({item}) => 
-                      <View style={styles.checkboxContainer}>
-                        <Text key={item.index}>{item.time}: {item.message} </Text>
-                      </View>}  
-                  />
+  return (
+    <View style={styles.container}>
+      {gameBegun ? (
+        <View>
+          <Text>The game has begun</Text>
+          <ScrollView style={styles.messagesContainer}>
+            <FlatList
+              data={adminMessages}
+              renderItem={({ item }) => (
+                <View style={styles.checkboxContainer}>
+                  <Text key={item.index}>
+                    {item.time}: {item.message}{' '}
+                  </Text>
                 </View>
+              )}
+            />
+          </ScrollView>
+        </View>
+      ) : room ? (
+        <View>
+          <Text>User: {name}</Text>
+          <Text>Room: {room}</Text>
+          <Button onPress={handleReady} title={!isReady ? 'Ready?' : 'Not Ready'} />
+          <Button onPress={handleLeave} title="Go out"></Button>
+          <Text>List of users:</Text>
+          <FlatList
+            data={users}
+            renderItem={({ item }) => (
+              <View style={styles.checkboxContainer}>
+                <Text key={item.index}>
+                  {item.index}: {item.name}{' '}
+                </Text>
+                <CheckBox disabled={true} style={styles.checkBox} checked={item.isReady} />
               </View>
-              :
-              <Formik
-                initialValues={{ room: this.state.room, name: this.state.name }}
-                onSubmit={values => this.onSubmit(values)}
-              >
-                {({ handleChange, handleBlur, handleSubmit, values }) => {
-                  return (
-                    <View>
-                      <Text>Room</Text>
-                      <TextInput
-                        onChangeText={handleChange('room')}
-                        onBlur={handleBlur('room')}
-                        value={values.room}
-                      />
-                      <Text>User</Text>
-                      <TextInput
-                        onChangeText={handleChange('name')}
-                        onBlur={handleBlur('name')}
-                        value={values.name}
-                      />
-                      <Button onPress={handleSubmit} title="Go" />
-                    </View>
-                  )
-                }}
-              </Formik>
-            }
-        
-      </View>
-    )
-  }
-}
+            )}
+          />
+          <View>
+            <FlatList
+              data={adminMessages}
+              renderItem={({ item }) => (
+                <View style={styles.checkboxContainer}>
+                  <Text key={item.index}>
+                    {item.time}: {item.message}{' '}
+                  </Text>
+                </View>
+              )}
+            />
+          </View>
+        </View>
+      ) : (
+        <Formik initialValues={{ room: room, name: name }} onSubmit={values => onSubmit(values)}>
+          {({ handleChange, handleBlur, handleSubmit, values }) => {
+            return (
+              <View>
+                <Text>Room</Text>
+                <TextInput onChangeText={handleChange('room')} onBlur={handleBlur('room')} value={values.room} />
+                <Text>User</Text>
+                <TextInput onChangeText={handleChange('name')} onBlur={handleBlur('name')} value={values.name} />
+                <Button onPress={handleSubmit} title="Go" />
+              </View>
+            );
+          }}
+        </Formik>
+      )}
+    </View>
+  );
+};
 
 HomeScreen.navigationOptions = {
   header: null,
@@ -177,15 +171,12 @@ function DevelopmentModeNotice() {
 
     return (
       <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use useful development
-        tools. {learnMoreButton}
+        Development mode is enabled: your app will be slower but you can use useful development tools. {learnMoreButton}
       </Text>
     );
   } else {
     return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
+      <Text style={styles.developmentModeText}>You are not in development mode: your app will run at full speed.</Text>
     );
   }
 }
@@ -196,7 +187,7 @@ function handleLearnMorePress() {
 
 function handleHelpPress() {
   WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
+    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change',
   );
 }
 
@@ -206,13 +197,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   messagesContainer: {
-    height: 250
+    height: 250,
   },
   checkbox: {
-    alignSelf: "center",
+    alignSelf: 'center',
   },
   checkboxContainer: {
-    flexDirection: "row",
+    flexDirection: 'row',
     marginBottom: 20,
   },
   developmentModeText: {
@@ -298,3 +289,5 @@ const styles = StyleSheet.create({
     color: '#2e78b7',
   },
 });
+
+export default HomeScreen;
