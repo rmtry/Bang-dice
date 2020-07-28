19c94f956cd592c94b983bfa6f8dc0d38bb957cb
minh <shenlong2361996@gmail.com>
2020-07-29 00:28:36 +0200

    change ip

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -21,7 +21,7 @@ import { Formik } from 'formik';
 import { MonoText } from '../components/StyledText';
 
 import io from 'socket.io-client/dist/socket.io';
-const socket = io('http://192.168.0.179:3000'); // your router ip address here instead of localhost
+const socket = io('http://127.0.0.1:3000'); // your router ip address here instead of localhost
 
 const HomeScreen = props => {
   const [gameBegun, setGameBegun] = useState(false);
@@ -32,7 +32,7 @@ const HomeScreen = props => {
   const [users, setUsers] = useState([]);
   const [isReady, setIsReady] = useState(false);
   const [gameData, setGameData] = useState(undefined);
-  const [player, setPlayer] = useState(undefined)
+  const [player, setPlayer] = useState(undefined);
 
   /* setMessages = message => {
       setState(prevState => ({ adminMessages: prev   adminMessages.concat([message]) }));
@@ -71,17 +71,16 @@ const HomeScreen = props => {
     // socket to receive game data
     socket.on('gameData', res => {
       setGameBegun(true);
-      if(res) setGameData(res)
-      console.log(res)
-      if(player === undefined) setPlayer(res.players.find(player => player.userId === socket.id)) 
+      if (res) setGameData(res);
+      console.log(res);
+      if (player === undefined) setPlayer(res.players.find(player => player.userId === socket.id));
     });
   }, []);
-
 
   const handleLeave = () => {
     socket.emit('leave', { room: room, name: name }, () => {
       setRoom(undefined);
-      setAdminMessages([])
+      setAdminMessages([]);
     });
   };
 
@@ -89,12 +88,9 @@ const HomeScreen = props => {
     socket.emit('ready', { id: socket.id, room: room, isReady: !isReady }, () => {});
   };
 
-  const handleAction = (type) => {
-    socket.emit('action', {room: room, action: type, name: name, index: gameData.players[0].index}, () => {
-      
-    })
-  }
-
+  const handleAction = type => {
+    socket.emit('action', { room: room, action: type, name: name, index: gameData.players[0].index }, () => {});
+  };
 
   const onSubmit = values => {
     console.log('on submit', values);
@@ -110,21 +106,36 @@ const HomeScreen = props => {
     <View style={styles.container}>
       {gameBegun ? (
         <View styles={styles.roomContainer}>
-            <View styles={styles.dash}></View>
-           {
-            gameData && (
-              <View styles={styles.playerWrapper}>
-                <Text>{gameData.dices[0].face}, {gameData.dices[0].rolled && 'saved!'}</Text>
-                <Text>{gameData.dices[1].face}, {gameData.dices[1].rolled && 'saved!'}</Text>
-                <Text>{gameData.dices[2].face}, {gameData.dices[2].rolled && 'saved!'}</Text>
-                <Text>{gameData.dices[3].face}, {gameData.dices[3].rolled && 'saved!'}</Text>
-                <Text>{gameData.dices[4].face}, {gameData.dices[4].rolled && 'saved!'}</Text>
-                <Button onPress={() => handleAction('ROLLDICE')} title={'Roll'} disabled={(gameData.turnRoll > 2 && player && player.index === gameData.currentTurnIndex) || player && player.index !== gameData.currentTurnIndex}></Button>
-                <Button onPress={() => handleAction('keep')} title={'Save'}></Button>
-                <Text>{player !== undefined && player.index}</Text>
-              </View>
-            )
-          }
+          <View styles={styles.dash}></View>
+          {gameData && (
+            <View styles={styles.playerWrapper}>
+              <Text>
+                {gameData.dices[0].face}, {gameData.dices[0].rolled && 'saved!'}
+              </Text>
+              <Text>
+                {gameData.dices[1].face}, {gameData.dices[1].rolled && 'saved!'}
+              </Text>
+              <Text>
+                {gameData.dices[2].face}, {gameData.dices[2].rolled && 'saved!'}
+              </Text>
+              <Text>
+                {gameData.dices[3].face}, {gameData.dices[3].rolled && 'saved!'}
+              </Text>
+              <Text>
+                {gameData.dices[4].face}, {gameData.dices[4].rolled && 'saved!'}
+              </Text>
+              <Button
+                onPress={() => handleAction('ROLLDICE')}
+                title={'Roll'}
+                disabled={
+                  (gameData.turnRoll > 2 && player && player.index === gameData.currentTurnIndex) ||
+                  (player && player.index !== gameData.currentTurnIndex)
+                }
+              ></Button>
+              <Button onPress={() => handleAction('keep')} title={'Save'}></Button>
+              <Text>{player !== undefined && player.index}</Text>
+            </View>
+          )}
           <View styles={styles.gameWrapper}>
             <ScrollView style={styles.messagesWrapper}>
               <FlatList
@@ -149,14 +160,18 @@ const HomeScreen = props => {
           <View style={styles.playerWrapper}>
             <Text style={styles.textTitle}>User: {name}</Text>
             <View style={styles.miniForm}>
-              <TouchableOpacity style={styles.button} onPress={handleReady} ><Text style={styles.buttonTitle}>{!isReady ? 'READY?' : 'NOT READY'}</Text></TouchableOpacity>
-              <TouchableOpacity style={styles.buttonCancel} onPress={handleLeave} ><Text style={styles.buttonTitleCancel}>LEAVE</Text></TouchableOpacity>
+              <TouchableOpacity style={styles.button} onPress={handleReady}>
+                <Text style={styles.buttonTitle}>{!isReady ? 'READY?' : 'NOT READY'}</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.buttonCancel} onPress={handleLeave}>
+                <Text style={styles.buttonTitleCancel}>LEAVE</Text>
+              </TouchableOpacity>
             </View>
           </View>
-          
+
           <View style={styles.gameWrapper}>
             <View style={styles.usersWrapper}>
-              { users.map(item => (
+              {users.map(item => (
                 <View style={styles[`userWrapper${item.index > 4 ? 'Right' : 'Left'}`]}>
                   <Text styles={styles.TextInput} key={item.index} key={item.index}>
                     {item.index}: {item.name}{' '}
@@ -180,24 +195,34 @@ const HomeScreen = props => {
           </View>
 
           <View style={styles.headerWrapper}>
-              <Text style={styles.textTitle}>Waiting Room [{room}]</Text>
+            <Text style={styles.textTitle}>Waiting Room [{room}]</Text>
           </View>
         </View>
       ) : (
         <View style={styles.roomContainer}>
-          <View style={styles.playerWrapper}>
-            
-          </View>
+          <View style={styles.playerWrapper}></View>
           <View style={styles.gameWrapper}>
             <Formik initialValues={{ room: room, name: name }} onSubmit={values => onSubmit(values)}>
               {({ handleChange, handleBlur, handleSubmit, values }) => {
                 return (
                   <View style={styles.miniForm}>
                     <Text style={styles.inputLabel}>Room</Text>
-                    <TextInput style={styles.textInput} onChangeText={handleChange('room')} onBlur={handleBlur('room')} value={values.room} />
+                    <TextInput
+                      style={styles.textInput}
+                      onChangeText={handleChange('room')}
+                      onBlur={handleBlur('room')}
+                      value={values.room}
+                    />
                     <Text style={styles.inputLabel}>User</Text>
-                    <TextInput style={styles.textInput} onChangeText={handleChange('name')} onBlur={handleBlur('name')} value={values.name} />
-                    <TouchableOpacity onPress={handleSubmit} style={styles.button}><Text style={styles.buttonTitle}>GO</Text></TouchableOpacity>
+                    <TextInput
+                      style={styles.textInput}
+                      onChangeText={handleChange('name')}
+                      onBlur={handleBlur('name')}
+                      value={values.name}
+                    />
+                    <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+                      <Text style={styles.buttonTitle}>GO</Text>
+                    </TouchableOpacity>
                   </View>
                 );
               }}
@@ -250,20 +275,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    color: '#575757'
+    color: '#575757',
   },
   dash: {
     height: 10,
     width: 250,
     alignSelf: 'center',
-    backgroundColor: 'grey'
+    backgroundColor: 'grey',
   },
   messagesContainer: {
     height: 250,
   },
   roomContainer: {
     flex: 1,
-    flexDirection: 'column-reverse'
+    flexDirection: 'column-reverse',
   },
   playerWrapper: {
     flex: 3,
@@ -272,23 +297,23 @@ const styles = StyleSheet.create({
   gameWrapper: {
     flex: 6,
     padding: 8,
-    alignItems:'stretch'
+    alignItems: 'stretch',
   },
   headerWrapper: {
     flex: 1,
     alignSelf: 'center',
-    paddingTop: '2.5%'
+    paddingTop: '2.5%',
   },
-  usersWrapper:{
+  usersWrapper: {
     flexDirection: 'column',
     flexWrap: 'wrap',
     alignContent: 'space-between',
     height: 200,
     ...Platform.select({
-      web:{
-        height: '40%'
-      }
-    })
+      web: {
+        height: '40%',
+      },
+    }),
   },
   userWrapperLeft: {
     height: 50,
@@ -298,11 +323,11 @@ const styles = StyleSheet.create({
     //backgroundColor: 'grey',
     flexDirection: 'row',
     ...Platform.select({
-      web:{
+      web: {
         height: '25%',
-        width: '50%'
-      }
-    })
+        width: '50%',
+      },
+    }),
   },
   userWrapperRight: {
     height: 50,
@@ -312,12 +337,12 @@ const styles = StyleSheet.create({
     //backgroundColor: 'grey',
     flexDirection: 'row',
     ...Platform.select({
-      web:{
+      web: {
         height: '25%',
-        width: '50%'
-      }
+        width: '50%',
+      },
     }),
-    justifyContent: 'flex-end'
+    justifyContent: 'flex-end',
   },
   checkbox: {
     alignSelf: 'center',
@@ -327,7 +352,7 @@ const styles = StyleSheet.create({
     margin: 8,
     marginLeft: 0,
     marginRight: 0,
-    color: '#575757'
+    color: '#575757',
   },
   textInput: {
     fontSize: 14,
@@ -335,18 +360,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#ebebeb',
     paddingLeft: 12,
     marginBottom: 8,
-    borderRadius: 10
+    borderRadius: 10,
   },
   textMessage: {
     fontSize: 12,
-    marginBottom: 4
+    marginBottom: 4,
   },
   boldText: {
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   inputLabel: {
     color: 'grey',
-    fontSize: 12
+    fontSize: 12,
   },
   button: {
     height: 52,
@@ -354,7 +379,7 @@ const styles = StyleSheet.create({
     padding: 12,
     marginTop: 12,
     borderRadius: 10,
-    alignItems: 'center'
+    alignItems: 'center',
   },
   buttonCancel: {
     height: 52,
@@ -364,31 +389,31 @@ const styles = StyleSheet.create({
     marginTop: 12,
     borderRadius: 10,
     alignItems: 'center',
-    borderWidth: 0.5
+    borderWidth: 0.5,
   },
   buttonTitle: {
     fontSize: 16,
     margin: 'auto',
-    color: 'white'
+    color: 'white',
   },
   buttonTitleCancel: {
     fontSize: 16,
     margin: 'auto',
-    color: '#2196F3'
+    color: '#2196F3',
   },
   miniForm: {
     ...Platform.select({
       web: {
         width: '50%',
         alignSelf: 'center',
-      }
-    })
+      },
+    }),
   },
-  messagesContainer:{
+  messagesContainer: {
     backgroundColor: '#ebebeb',
     borderRadius: 10,
     padding: 12,
-    marginTop: 8
+    marginTop: 8,
   },
   developmentModeText: {
     marginBottom: 20,
